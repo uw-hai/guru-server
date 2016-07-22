2875efd6a21a5aa32a2d65741f91e2343a747624
Jonathan Bragg <jbragg@cs.washington.edu>
2016-07-22 15:17:46 -0700

    Logging

--- guruserver/app.py
+++ guruserver/app.py
@@ -1,11 +1,16 @@
 """app.py"""
 import os
+import sys
+import logging
 from flask import Flask
 from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 db = MongoEngine(app)
+
+logging.basicConfig(
+    stream=app.config['LOGSTREAM'], level=app.config['LOGLEVEL'])
 
 TMP_DIR = os.environ.get('TMP_DIR', os.path.join('/tmp', 'guruserver'))
 
--- guruserver/config.py
+++ guruserver/config.py
@@ -1,17 +1,23 @@
 import os
+import sys
+import logging
 
 
 class Config(object):
     MONGODB_HOST = os.environ['MONGODB_URL']
+    LOGSTREAM = sys.stdout
 
 class DevelopmentConfig(Config):
     DEBUG = True
     TESTING = True
+    LOGLEVEL = logging.DEBUG
 
 class TestingConfig(Config):
     DEBUG = True
     TESTING = True
+    LOGLEVEL = logging.INFO
 
 class ProductionConfig(Config):
     DEBUG = False
     TESTING = False
+    LOGLEVEL = logging.ERROR
